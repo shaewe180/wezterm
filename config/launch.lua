b3e123420ba722dc7646dab54ed016ec358c0328
shaewe180 <shaewe180@gmail.com>
2025-08-11 08:51:30 +0800

    rm some

--- config/launch.lua
+++ config/launch.lua
@@ -8,26 +8,18 @@ local options = {
 if platform.is_win then
   options.default_prog = { "powershell" }
   options.launch_menu = {
-    { label = " PowerShell v1", args = { "powershell" } },
-    { label = " PowerShell v7", args = { "pwsh" } },
     { label = " Cmd", args = { "cmd" } },
-    { label = " Nushell", args = { "nu" } }
   }
 elseif platform.is_mac then
   options.default_prog = { "zsh", "--login" }
   options.launch_menu = {
     { label = " Bash", args = { "bash", "--login" } },
-    { label = " Fish", args = { "fish", "--login" } },
-    { label = " Nushell", args = { "nu", "--login" } },
     { label = " Zsh", args = { "zsh", "--login" } },
   }
 elseif platform.is_linux then
   options.default_prog = { "bash", "--login" }
   options.launch_menu = {
     { label = " Bash", args = { "bash", "--login" } },
-    { label = " Fish", args = { "fish", "--login" } },
-    { label = " Nushell", args = { "nu", "--login" } },
-    { label = " Zsh", args = { "zsh", "--login" } },
   }
 end
 
